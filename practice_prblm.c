38958d8524620e8b8297fd94cbc2c5b1b787843e
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-04-13 16:05:25 +0600

    Update practice_prblm.c

---
+++ practice_prblm.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+    //I. Welcome for you with Conditions 
+
+int a,b;
+scanf("%d%d",&a,&b);
+
+if(a>=b){
+    printf("Yes\n");
+}
+else{
+    printf("No\n");
+}
+
+return 0;
+}
